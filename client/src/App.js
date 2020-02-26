ea2946b7d51cc3df4ccbc29dacef2f927060f331
AndresGutierrez <agutierrez.pers@gmail.com>
2020-02-26 07:48:09 +0100

    -Cambios cap. 34. Carga rutas LayoutAdmin.

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import routes from "./config/routes";
 
 import "./App.scss";
-import { render } from "node-sass";
+/*/ import { render } from "node-sass"; */
 
 function App() {
   return (
--- client/src/layouts/LayoutAdmin.js
+++ client/src/layouts/LayoutAdmin.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import {Layout} from 'antd';
+import React from "react";
+import { Route } from "react-router-dom";
+import { Layout } from "antd";
 
-export default function LayoutAdmin(){
+import "./LayoutAdmin.scss";
+
+export default function LayoutAdmin(props){
+    const { routes } = props;
+    const { Header, Content, Footer } = Layout;
+    console.log(props);
+
     return (
         <Layout>
             <h2>Menu Sider Admin</h2>
-            <div>Contenido.....</div>
-            <h5>Footer...</h5>
+            <Layout>
+                <Header>Header....</Header>
+                <Content>...Rutas...
+					<LoadRoutes routes={routes}/>
+				</Content>
+                <Footer>Andrés Gutiérrez</Footer>
+            </Layout>
         </Layout>
     );
+}
+
+function LoadRoutes({ routes } ){
+	return routes.map((route, index) => (
+		<Route
+			key={index}
+			path={route.path}
+			exact={route.exact}
+			component={route.component}
+        />
+    ));
 }
