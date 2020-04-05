8545fcf77c5aa9bfc540a9ffd4b8f983a514b5e2
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-05 21:49:27 +0200

    Guardando los tokens en el LocalStorage.

--- client/src/components/Admin/LoginForm/LoginForm.js
+++ client/src/components/Admin/LoginForm/LoginForm.js
@@ -1,6 +1,7 @@
 import React, { useState }  from 'react';
 import { Form, Icon, Input, Button, notification } from 'antd';
 import { signInApi }  from '../../../api/user';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../../../utils/constants';
 
 import "./LoginForm.scss";
 
@@ -17,11 +18,28 @@ export default function LoginForm() {
         });
     };
 
-    const login = e => {
+    const login = async e => {
         e.preventDefault();
 
-        signInApi(inputs);
-        console.log(inputs);
+        const result = await signInApi(inputs);
+
+        if (result.message) {
+            notification["error"]({
+                message: result.message
+            });
+        } else {
+            const { accessToken, refreshToken } = result;
+            localStorage.setItem(ACCESS_TOKEN, accessToken);
+            localStorage.setItem(REFRESH_TOKEN, refreshToken);
+
+            notification["success"]({
+                message: "Login correcto."
+            });
+
+            window.location.href = "/admin";
+        }
+
+        console.log(result);
     };
 
     return  (
