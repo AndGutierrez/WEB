a351c7f2c1623e6f94b1fd6ea169563fa2220bba
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-11 11:43:12 +0200

    Cambios para filtrar usuarios activos e inactivos.

--- client/src/pages/Admin/Users/Users.js
+++ client/src/pages/Admin/Users/Users.js
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { getAccessTokenApi } from '../../../api/auth';
-import { getUsersApi } from '../../../api/user';
+import { getUsersActiveApi } from '../../../api/user';
 
 import "./Users.scss";
 
 export default function Users() {
-    const [users, setUsers] = useState([]);
+    const [usersActive, setUsersActive] = useState([]);
+    const [usersInactive, setUsersInactive] = useState([]);
     const token = getAccessTokenApi();
 
-    console.log(users);
+    console.log("Usuario activos: ", usersActive);
+    console.log("Usuario inactivos: ", usersInactive);
     
     useEffect(() => {
-        getUsersApi(token).then((response) => {
-            setUsers(response);
+        getUsersActiveApi(token, true).then((response) => {
+            setUsersActive(response);
+        });
+        getUsersActiveApi(token, false).then((response) => {
+            setUsersInactive(response);
         });
     }, [token]);
 
