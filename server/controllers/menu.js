ce850ef39a37def9f57b6033e0f7bcab2b911fae
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-18 12:31:32 +0200

    Endpoint para obtener usuarios.

--- server/controllers/menu.js
+++ server/controllers/menu.js
@@ -1,9 +1,42 @@
 const Menu = require("../models/menu");
 
 function addMenu(req, res) {
-    console.log("Add Menú");    
+    const { title, url, order, active } = req.body; 
+    const menu = new Menu();
+    menu.title = title;
+    menu.url = url;
+    menu.order = order;
+    menu.active = active;
+
+    menu.save((err, createdMenu) => {
+        if (err) {
+            res.status(500).send({ message: "Error del servidor." });
+        } else {
+            if (!createdMenu) {
+                res.status(404).send({ message: "Error al crear el menú." });
+            }
+            res.status(200).send({ message: "Menú creado correctamente." });
+        }
+    });
+}
+
+function getMenus(req, res) {    
+    Menu.find()
+    .sort({ order: "asc" })
+    .exec((err, menus) => {
+        if (err){
+            res.status(500).send({ message: "Error del servidor." });
+        } else {
+            if (!menus) {
+                res.status(404).send({ message: "No se ha encontrado ningún menú." });
+            } else {
+                res.status(200).send({ menus });
+            }
+        } 
+    });
 }
 
 module.exports = {
     addMenu,
+    getMenus,
 };
