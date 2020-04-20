64a2801db8e2e9007830bccd2ad2458613ac0ad7
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-20 22:25:14 +0200

    Creando los elementos del MenuTop de forma dinámica y que abren enlaces.

--- client/src/components/Web/MenuTop/MenuTop.js
+++ client/src/components/Web/MenuTop/MenuTop.js
@@ -1,26 +1,56 @@
 import React, { useState, useEffect } from 'react';
 import { Menu } from 'antd';
 import { Link } from 'react-router-dom';
+import { getMenusApi } from '../../../api/menu';
+import { getAccessTokenApi } from '../../../api/auth';
 import logo from '../../../assets/img/png/adminlogo.png';
 
 import './MenuTop.scss';
 
 export default function MenuTop() {
+    const [menuData, setMenuData] = useState([]);
+
+    useEffect(() => {
+        const accessToken = getAccessTokenApi();
+
+        getMenusApi(accessToken).then(response => {
+            const arrayMenu = [];
+            response.menus.forEach(item => {
+                item.active && arrayMenu.push(item);
+            });
+            setMenuData(arrayMenu);
+        });
+    }, []);
+
     return (
-        <Menu className="menu-top" mode="horizontal">
-            <Menu.Item className="menu-top__logo">
-                <Link to={"/"}>>
+        <Menu className="menu-top-web" mode="horizontal">
+            <Menu.Item className="menu-top-web__logo">
+                <Link to={"/"}>
                     <img src={logo} alt="Andrés Gutiérrez Vera"/>
                 </Link>
             </Menu.Item>
-            
-            <Menu.Item className="menu-top__item">
-                <Link to={"/"}>Home</Link>
-            </Menu.Item>
 
-            <Menu.Item className="menu-top__item">
-                <Link to={"/contact"}>Contacto</Link>
-            </Menu.Item>
+            {menuData.map(item => {
+                const external = item.url.indexOf("http") > -1 ? true : false;
+
+                if (external) {
+                    return (
+                        <Menu.Item className="menu-top-web__item" key={item._id}>
+                                <a href={item.url} target="_blank" rel="noopener noreferrer">
+                                    {item.title}
+                                </a>
+                            </Menu.Item>
+                    );
+                } else {
+                    return (
+                        <Menu.Item className="menu-top-web__item" key={item._id}>
+                            <Link to={item.url}>
+                                {item.title}
+                            </Link> 
+                        </Menu.Item>
+                    );
+                }
+            })}
 
             <div>
                 Social Media...
