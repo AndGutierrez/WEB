3d8c57c00576bcced9d5cdf505fcdde2694854ab
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-23 09:56:26 +0200

    Enviamos el email para suscirbirnos al newsletter.

--- client/src/components/Web/Newsletter/Newsletter.js
+++ client/src/components/Web/Newsletter/Newsletter.js
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Icon, Input, Button, notification } from 'antd';
+import { suscribeNewsletterApi } from '../../../api/newsletter'
 
 import './Newsletter.scss';
 
 export default function Newsletter() {
+    const [email, setEmail] = useState("");
+
     const onSubmit = e => {
         e.preventDefault();
-        console.log("Newsletter enviado");        
+        
+        const emailValid = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        const resultValidation = emailValid.test(email);
+
+        if(!resultValidation) {
+            notification["error"]({ message: "Correo electrónico no válido." });
+            return;
+        }
+        
+        suscribeNewsletterApi(email).then(response => {
+            if (response.code == 200) {
+                notification["success"]({ message: response.message });
+                setEmail("");
+            } else {
+                notification["warning"]({ message: response.message });
+                return;
+            }
+        })
     };
 
     return (
@@ -16,8 +36,8 @@ export default function Newsletter() {
                 <Form.Item>
                     <Input prefix={<Icon type="user" style={{ color: "rgba(0, 0, 0, 0.25" }} />} 
                         placeholder="Correo electrónico"
-                        // value={}
-                        // onChange={}
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
                     />
                 </Form.Item>
                 <Form.Item>
