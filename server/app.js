9e1b64c29b12a2279c082733e6bb7f6f577c5a73
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-24 08:35:54 +0200

    Añadimos endpoint para cursos. Cambiamos nombre carpeta de midellwares.

--- server/app.js
+++ server/app.js
@@ -9,6 +9,8 @@ const authRoutes = require("./routers/auth");
 const userRoutes = require("./routers/user");
 const menuRoutes = require("./routers/menu");
 const newsletterRoutes = require('./routers/newsletter');
+const courseRoutes = require("./routers/course");
+
 // ....
 
 app.use(bodyParser.urlencoded({ extended: false}));
@@ -32,7 +34,6 @@ app.use(`/api/${API_VERSION}`, userRoutes);
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, menuRoutes);
 app.use(`/api/${API_VERSION}`, newsletterRoutes);
-
-// ....
+app.use(`/api/${API_VERSION}`, courseRoutes);
 
 module.exports = app;
--- server/routers/menu.js
+++ server/routers/menu.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const MenuController = require('../controllers/menu');
 
-const md_auth = require('../middleware/authenticated');
+const md_auth = require('../middlewares/authenticated');
 
 const api = express.Router();
 
