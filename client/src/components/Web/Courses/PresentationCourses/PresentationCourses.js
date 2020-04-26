5a52478dceecc47461f094034189cbf7de8be44b
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-26 11:53:58 +0200

    Componente de presentación de los cursos.

--- client/src/components/Web/Courses/PresentationCourses/PresentationCourses.js
+++ client/src/components/Web/Courses/PresentationCourses/PresentationCourses.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import AcademyLogo from '../../../../assets/img/png/academy-logo.png';
 
 import './PresentationCourses.scss';
 
 export default function PresentationCourses() {
     return (
         <div className="presentation-courses">
-            <h2>PresentationCourses...</h2>
+            <img src={AcademyLogo} alt="Cursos disponibles"/>
+            <p>Vas a encontrar los mejores cursos online de desarrollo web en Español. Unete a nosotros y 
+                empieza tu camino como Desarrollador Web o Desarrollador de CMS. Sinceramente, estos cursos
+                contienen  el tipo de contenido que a mi me hubiera gustado encontrar cuando empecé en
+                el mundo del desarrollo web profesional.
+            </p>
+            <p>¡¡¡Échales un vistazo y aprovecha las ofertas!!!</p>
         </div>
     );
 }
