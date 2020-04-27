0b5628a5200443a72ff551a24c389ec8e94a8d95
AndresGutierrez <agutierrez.pers@gmail.com>
2020-04-27 08:59:35 +0200

    Añadimos tarjetas con toda la información de cada curso.

--- client/src/components/Web/Courses/CoursesList/CoursesList.js
+++ client/src/components/Web/Courses/CoursesList/CoursesList.js
@@ -1,11 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Card, Button, Rate, notification } from 'antd';
+import { getCourseDataUdemyApi } from '../../../../api/course';
 
 import './CoursesList.scss';
 
-export default function CoursesList() {
+export default function CoursesList(props) {
+    const { courses } = props;
+    
     return (
         <div className="courses-list">
-            <h2>CoursesList...</h2>
+            <Row>
+                {courses.map(course => (
+                    <Col md={8} className="courses-list__course">
+                        <Course key={course._id} course={course}/>
+                    </Col>
+                ))}
+            </Row>
         </div>
+    );
+}
+
+function Course(props) {
+    const { course } = props;
+    const [courseInfo, setCourseInfo] = useState({});
+    const [urlCourse, setUrlCourse] = useState(null);
+    const { Meta } = Card;
+
+    useEffect(() => {
+        getCourseDataUdemyApi(course.idCourse).then(response => {
+            if (response?.code !== 200) {
+                notification["warning"]({ message: response.message });    
+            } else {               
+                setCourseInfo(response.data);
+                mountUrl(response.data.url);
+            }
+        })
+        .catch(() => {
+            notification["error"]({ message: "Error del servidor, intentelo más tarde." });
+        });
+    }, [course]);
+
+    const mountUrl = url => {
+        if (!course.link) {
+           const baseUrl = `https://www.udemy.com${url}`;
+           const urlUdemyCourse = baseUrl + (course.coupon ? `?couponCode=${course.coupon}` : "");
+           setUrlCourse(urlUdemyCourse);
+        } else 
+        {
+            setUrlCourse(course.link);
+        }
+    };
+
+    return (
+        <a href={urlCourse} target="_blank" rel="nooperner noreferrer">
+            <Card 
+                cover={<img src={courseInfo.image_480x270} alt={courseInfo.title}/>}
+            >
+                <Meta title={courseInfo.title} description={courseInfo.headline}/>
+                <Button>Acceder al curso</Button>
+
+                <div className="courses-list__course-footer">
+                    <span>{course.price ? `€ ${course.price}` : courseInfo.price}</span>
+                    <div>
+                        <Rate disabled defaultValue={5} />
+                    </div>
+                </div>
+            </Card>
+        </a>
     );
 }
--- client/src/pages/Courses.js
+++ client/src/pages/Courses.js
@@ -6,12 +6,35 @@ import CoursesList from '../components/Web/Courses/CoursesList';
 import { getCoursesApi } from '../api/course';
 
 export default function Courses() {
+    const [courses, setCourses] = useState(null);
+    
+    useEffect(() => {
+        getCoursesApi()
+        .then(response => {
+            if(response?.code !== 200) {
+                notification["warning"]({ message: response.message });
+            } else {
+                setCourses(response.courses);
+            }
+        })
+        .catch(() => {
+            notification["warning"]({ message: "Error del servidor, intentelo más tarde." });
+        });
+        
+    }, []);
+
     return (
         <Row>
             <Col md={4}/>
             <Col md={16}>
                 <PresentationCourses/>
-                <CoursesList/>
+                {!courses ? (
+                    <Spin 
+                        tip="Cargando cursos" 
+                        style={{ textAlign: "center", width: "100%", padding: "20px" }}/>
+                ) : (
+                    <CoursesList courses={courses}/>
+                )}                
             </Col>
             <Col md={4}/>
         </Row>
